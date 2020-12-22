b4dedaa7d8fb2feeea2b8cb7327d1491dd04539f
Pedro Peñalver Yusta <pedropenalver@packlink.com>
2020-12-22 20:05:08 +0100

    fix: forgotten apps

--- scripts/development.sh
+++ scripts/development.sh
@@ -4,8 +4,13 @@
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo /bin/sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
+
+sudo dnf install -y code haskell-platform meld code
+
 # Install n for npm
 curl -L https://git.io/n-install | bash -s -- -y
+
+source ~/.zshrc
 
 npm i -g yarn
 npm i -g serve
