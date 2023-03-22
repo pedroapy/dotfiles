ded8c82416f7745092d9ac94f829a639afc89f49
Pedro Peñalver Yusta <pedro.penalver@auctane.com>
2023-03-22 12:50:51 +0100

    feat: update

--- scripts/polybar.sh
+++ scripts/polybar.sh
@@ -2,10 +2,11 @@
 
 # install polybar
 #############################
-yay -S --noconfirm polybar polybar-spotify-module pulseaudio-control wedder i3-agenda
+yay -S --noconfirm polybar polybar-spotify-module pulseaudio-control wedder i3-agenda ttf-all-the-icons
 
 mkdir -p ~/.polybar/plugins
-git clone https://github.com/meelkor/polybar-i3-windows.git ~/.polybar/plugins/polybar-i3-windows
+git clone https://github.com/meelkor/polybar-i3-windows.git $HOME/.polybar/plugins/polybar-i3-windows
+
 
 git clone https://github.com/firecat53/networkmanager-dmenu.git /tmp/networkmanager_dmenu
 cp /tmp/networkmanager_dmenu/networkmanager_dmenu $HOME/bin
@@ -16,8 +17,8 @@ cd /tmp/siji
 cd -
 
 ln -sv $HOME/dotfiles/config/polybar $HOME/.config/polybar
-git clone https://github.com/polybar/polybar-scripts.git ~/polybar-scripts
-chmod u+x ~/polybar-scripts/**/*.sh
+git clone https://github.com/polybar/polybar-scripts.git $HOME/polybar-scripts
+chmod u+x $HOME/polybar-scripts/**/*.sh
 
 ln -sv $HOME/dotfiles/config/wedder $HOME/.config/wedder
 
