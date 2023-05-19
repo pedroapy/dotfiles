f43d5de1937294f9862e217bdca3ff506f9ce641
pedroapy <ppenalver@lang.ai>
2023-05-19 17:00:25 +0200

    feat: remove i3 and xorg

--- bin/get-wallpaper.sh
+++ bin/get-wallpaper.sh
@@ -13,15 +13,4 @@ echo "saved to $filename"
 # Download wallpaper to file
 curl "https://www.bing.com$urlpath" -o "$filename"
 
-if [ "$XDG_CURRENT_DESKTOP" = "i3" ]; then
-    # Set wallpaper
-    feh --bg-fill "$filename"
-    # Set lockscreen
-    betterlockscreen -u $filename --blur 0.5
-
-elif [ "$XDG_CURRENT_DESKTOP" = "sway" ]; then
-    swaymsg output "*" bg $filename fill
-
-else
-    echo "Estás en un gestor de ventanas desconocido"
-fi
+swaymsg output "*" bg $filename fill
--- config/polybar/scripts/nodever.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-output="$($HOME/n/bin/node -v)"
-
-
-echo "$output"
